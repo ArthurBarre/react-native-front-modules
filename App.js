4090cda94820de763b0cf40aa7a68d72cbcc0155
ArthurBArre <arthur.barre@hetic.net>
2019-07-02 16:25:16 +0200

    organize tuto files

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 //import PresentationalComponent from './components/PresentationalComponent'
-import FlexBox from './components/FlexBox'
-
+//import FlexBox from './components/FlexBox'
+import List from './components/List';
 class App extends React.Component {
   render() {
     return (
       <View>
-        <FlexBox />
+        <List />
       </View>
     );
   }
